da6495b7205ce28f748fc242fc2e217eb5ed1342
SaltyCrew19 <Saltycrew19@outlook.com>
2026-07-20 18:46:43 -0600

    add: implement Player class with constructors and getters/setters for position, money, and jail status

--- project4/src/Player.cpp
+++ project4/src/Player.cpp
@@ -1,1 +1,17 @@
 //Player.cpp
+#include "Player.h"
+
+Player::Player() : name("Default Name"), style(Playstyle::Neutral){}
+Player::Player(std::string name):name(std::move(name)),style(Playstyle::Neutral) {}
+Player::Player(std::string name, Playstyle style):name(std::move(name)),style(style) {}
+
+
+
+
+//getters and setters
+    void Player::setPos(int pos){this->pos = pos;}
+    int Player::getPos() const {return this->pos;}
+    void Player::setMoney(int money){this->money = money;}
+    int Player::getMoney() const {return this->money;}
+    void Player::setInJail(bool inJail){this->inJail = inJail;}
+    bool Player::getInJail() const {return this->inJail;}
--- project4/src/header/Player.h
+++ project4/src/header/Player.h
@@ -1,1 +1,38 @@
-////Player.h
+// Player.h
+#pragma once
+
+#include <vector>
+#include <string>
+#include "Card.h"
+enum class Playstyle
+{
+    Neutral,
+    Aggressive,
+    Carefull
+};
+class Player
+{
+private:
+    std::string name;
+    Playstyle style;
+    int pos = 0;
+    int money = 1500;
+    bool inJail = false;
+    std::vector<Card> cards;
+public:
+
+    bool isDone = false;
+    int place = 0;
+    Player();
+    Player(std::string name);
+    Player(std::string name, Playstyle style);
+
+    //getters and setters
+    void setPos(int pos);
+    int getPos() const;
+    void setMoney(int money);
+    int getMoney() const;
+    void setInJail(bool inJail);
+    bool getInJail() const;
+    
+};

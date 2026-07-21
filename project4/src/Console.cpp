b8c5df7b9eb5e0f379e7bc2d840a4f612c8b57c4
SaltyCrew19 <Saltycrew19@outlook.com>
2026-07-21 15:25:55 -0600

    fix: update Console class print and error methods to use newline character for consistency

--- project4/src/Console.cpp
+++ project4/src/Console.cpp
@@ -4,10 +4,10 @@
 Console::Console(){this->projectName = "[c++System] ";}
 Console::Console(std::string name){this -> projectName = "[" + name + "] ";}
 
-void Console::print(std::stringstream &msg){std::cout << projectName << msg.str() << std::endl;}
-void Console::print(const std::string &msg){std::cout << projectName << msg << std::endl;}
-void Console::error(std::stringstream &msg){std::cout << "-[ERROR]-" <<projectName << msg.str() << std::endl;}
-void Console::error(const std::string &msg){std::cout << "-[ERROR]-" <<projectName << msg << std::endl;}
+void Console::print(std::stringstream &msg){std::cout << projectName << msg.str() << "\n";}
+void Console::print(const std::string &msg){std::cout << projectName << msg << "\n";}
+void Console::error(std::stringstream &msg){std::cout << "-[ERROR]-" <<projectName << msg.str() << "\n";}
+void Console::error(const std::string &msg){std::cout << "-[ERROR]-" <<projectName << msg << "\n";}
 void Console::intro()
 {
     std::string intro;
